208e6ab65a7e3c3b63880e58a8f9eba15b80a6c7
Adogbeji Ibori <68833801+adomaster123@users.noreply.github.com>
2020-08-12 03:17:17 -0500

    Updates

---
+++ precache-manifest.60cefdbbc0e421f1a5558f399cf6a2f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76adc3ef562a52b32b658406abb1bb83",
+    "url": "/bug-tracker/index.html"
+  },
+  {
+    "revision": "e7672be18803cce5ff90",
+    "url": "/bug-tracker/static/js/2.a2584cb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bug-tracker/static/js/2.a2584cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c77543d12e018f603ff",
+    "url": "/bug-tracker/static/js/main.979ab5c7.chunk.js"
+  },
+  {
+    "revision": "a31a8ef397a4668de8f0",
+    "url": "/bug-tracker/static/js/runtime-main.890cf13e.js"
+  }
+]);
